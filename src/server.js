a755f9db28b7246960f8f71aaaad0905d1ff7f33
Pepa Labrin <catu.leibrin@gmail.com>
2024-06-19 20:21:48 -0400

    removed handlebars from server

--- src/server.js
+++ src/server.js
@@ -1,59 +1,35 @@
 import express from "express";
 import morgan from 'morgan';
 //import handlebars from 'express-handlebars';
-import { Server } from "socket.io";
-import viewsRouter from './src/routes/views.routes.js'
-import ProductsManager from "./src/managers/products.manager.js";
-import cartRouter from "./src/routes/carts.routes.js";
-import productsRouter from "./src/routes/products.routes.js";
+//import { Server } from "socket.io";
+//import viewsRouter from './routes/views.routes.js'
+import ProductsManager from "./managers/products.manager.js";
+import cartRouter from "./routes/carts.routes.js";
+import productsRouter from "./routes/products.routes.js";
+import userRouter from './routes/users.router.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { initMongoDB } from './db/database.js';
 
-import { __dirname } from "./src/path.js";
+// import { __dirname } from "./src/path.js";
 
-const productManager = new ProductsManager(`${__dirname}/db/products.json`)
+// const productManager = new ProductsManager(`${__dirname}/db/products.json`)
 
-const app = express()
+const app = express();
 
-app.use(express.static(__dirname + '/public'))
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(morgan('dev'));
 
-app.use('/api/carts', cartRouter)
-app.use('/api/products', productsRouter)
+app.use('/users', userRouter);
+app.use('/carts', cartRouter);
+app.use('/products', productsRouter);
 
+app.use(errorHandler);
 
-//tres líneas clave para handlebars
-app.engine('handlebars', handlebars.engine())
-app.set('views', `${__dirname}/views`)
-app.set('view engine', 'handlebars')
-
-app.use('/', viewsRouter)
+initMongoDB();
 
 const PORT = 8080;
 
-const httpServer = app.listen(PORT, () => {
+app.listen(PORT, () =>
     console.log(`Server ok on port ${PORT}`)
-});
-
-const socketServer = new Server(httpServer);
-
-socketServer.on('connection', async (socket) => {
-    console.log(`Nuevo cliente conectado - client ID: ${socket.id}`);
-
-    socket.emit('products', await productManager.getProducts());
-    console.log("Productos enviados al cliente");
-
-    socket.on('disconnect', () => console.log(`Cliente desconectado`));
-
-    socket.on('newProduct', async (newProduct) => {
-        productManager.addNewProduct(newProduct);
-        const products = await productManager.getProducts();
-        socketServer.emit('products', products)
-    });
-
-    socket.on('deleteProduct', async (id) => {
-        await productManager.deleteProduct(id)
-        console.log("producto eliminado")
-        const products = await productManager.getProducts();
-        socketServer.emit('products', products);
-    });
-})
+);
