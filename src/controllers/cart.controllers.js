38301934c9816230e6360615b94162a463c09ec4
Pepa Labrin <catu.leibrin@gmail.com>
2024-06-19 20:51:52 -0400

    added exports to cart.controllers.js

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -9,3 +9,105 @@ export const getAll = async (req, res, next) => {
     }
 };
 
+export const getById = async (req, res, next) => {
+    try{
+        const { id } = req.params;
+        const response = await service.getById(id);
+        if (!response) res.status(404).json({ msg: "Carrito no encontrado :c" });
+        else res.status(200).json(response);
+    } catch (error) {
+        next(error.message);
+    } 
+};
+
+export const create = async (req, res, next) => {
+    try {
+      const newCart = await service.create();
+      if (!newCart) res.status(404).json({ msg: "Error al crear el carrito :c" });
+      else res.status(200).json(newCart);
+    } catch (error) {
+      next(error.message);
+    }
+  };
+
+  export const update = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const cartUpd = await service.update(id, req.body);
+      if (!cartUpd) res.status(404).json({ msg: "Error al actualizar el carrito :c" });
+      else res.status(200).json(cartUpd);
+    } catch (error) {
+      next(error.message);
+    }
+  };
+
+  export const remove = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const cartDel = await service.remove(id);
+      if (!cartDel) res.status(404).json({ msg: "Error al borrar el carrito :c" });
+      else res.status(200).json({ msg: `Cart id: ${id} borrado` });
+    } catch (error) {
+      next(error.message);
+    }
+  };
+
+  export const addProdToCart = async (req, res, next) => {
+    try {
+      const { idCart } = req.params;
+      const { idProd } = req.params;
+      const newProdToUserCart = await service.addProdToCart(
+        idCart,
+        idProd,
+      );
+      if (!newProdToUserCart) res.json({ msg: "Error al añadir producto al carrito :c" });
+      else res.json(newProdToUserCart);
+    } catch (error) {
+      next(error.message);
+    }
+  };
+
+  export const removeProdToCart = async (req, res, next) => {
+    try {
+      const { idCart } = req.params;
+      const { idProd } = req.params;
+      const delProdToUserCart = await service.removeProdToCart(
+        idCart,
+        idProd,
+      );
+      if (!delProdToUserCart) res.json({ msg: "Error al sacar el producto del carrito :c" });
+      else res.json({msg: `product ${idProd} borrado del carrito`});
+    } catch (error) {
+      next(error.message);
+    }
+  };
+
+  export const updateProdQuantityToCart = async (req, res, next) => {
+    try {
+      const { idCart } = req.params;
+      const { idProd } = req.params;
+      const { quantity } = req.body;
+      const  updateProdQuantity = await service.updateProdQuantityToCart(
+        idCart,
+        idProd,
+        quantity
+      );
+      if (!updateProdQuantity) res.json({ msg: "Error al actualizar cantidad del producto en el carrito :c" });
+      else res.json(updateProdQuantity);
+    } catch (error) {
+      next(error.message);
+    }
+  };
+
+  export const clearCart = async (req, res, next) => {
+    try {
+      const { idCart } = req.params;
+      const clearCart = await service.clearCart(
+        idCart,
+      );
+      if (!clearCart) res.json({ msg: "Error al limpiar el carrito :c" });
+      else res.json(clearCart);
+    } catch (error) {
+      next(error.message);
+    }
+  };
